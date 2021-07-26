ad85484b654ba5529f407a0cc4c605562cc4969a
ndiagasarr17 <78365011+ndiagasarr17@users.noreply.github.com>
2021-07-26 14:51:10 +0000

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,7 @@
+public class Client {
+    public static void main(String[] args) {
+        ExpensiveObject object = new ExpensiveObjectProxy();
+        object.process();
+        object.process();
+    }
+}
