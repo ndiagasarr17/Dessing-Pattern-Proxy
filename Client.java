377bdb91f1130076633a5b149f4f3e9f817f2281
ndiagasarr17 <78365011+ndiagasarr17@users.noreply.github.com>
2021-07-26 14:57:13 +0000

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,3 +5,11 @@ public class Client {
         object.process();
     }
 }
+//Chaque fois que notre client appelle la méthode process(), il verra simplement le traitement et la configuration initiale restera toujours cachée
+
+
+
+//Notez que nous appelons la méthode process() deux fois.La partie des paramètres ne se produira qu'une seule fois 
+//- lorsque l'objet est initialisé pour la première fois.
+
+//Pour chaque autre appel suivant, ce modèle ignorera la configuration initiale et seul le traitement aura lieu 
